3c446a5a1e5ee7b8a70379a5b19afd944647ef6d
Avery Burne <burneav@gmail.com>
2021-08-04 22:01:33 -0400

    Added background game image

--- js/game-script.js
+++ js/game-script.js
@@ -1,9 +1,25 @@
 const gameboard = document.getElementById('game-canvas')
 const gameboardCtx = gameboard.getContext('2d')
+const background = new Image()
+
+background.src = "js/images/flappy-bird-background.png"
+
+function main() {
+    clearCanvas()
+    setTimeout(function onTick() {
+        clearCanvas()
+        main()
+    }, 100)
+}
+
+function drawBird() {
+
+}
 
 function clearCanvas() {
     gameboardCtx.fillStyle = 'white'
     gameboardCtx.strokestyle = 'black'
     gameboardCtx.fillRect(0, 0, gameboard.width, gameboard.height)
     gameboardCtx.strokeRect(0, 0, gameboard.width, gameboard.height)
+    gameboardCtx.drawImage(background,0,0)
 }
