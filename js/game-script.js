92197c7bae6f4d7bf88b7cf54c433a3f7f81eeab
Avery Burne <burneav@gmail.com>
2021-08-19 14:30:13 -0400

    Event listener for moving bird up
    Bird drops down every 0.1 seconds
    Main function runs each time a button is pressed, increases the speed with which the bird falls

--- js/game-script.js
+++ js/game-script.js
@@ -2,21 +2,26 @@ const gameboard = document.getElementById('game-canvas')
 const gameboardCtx = gameboard.getContext('2d')
 const background = new Image()
 const bird = new Image()
+let by = 200
 
 background.src = "js/images/flappy-bird-background.png"
 bird.src = "js/images/flappy-bird.png"
 
+document.addEventListener("keydown", moveBird)
 
 function main() {
     draw()
     setTimeout(function onTick() {
         draw()
         main()
+        by += 1
+        console.log(by)
     }, 100)
 }
 
-function drawBird() {
-
+function moveBird() {
+    by -= 50
+    console.log('hi')
 }
 
 function draw() {
@@ -25,5 +30,5 @@ function draw() {
     // gameboardCtx.fillRect(0, 0, gameboard.width, gameboard.height)
     // gameboardCtx.strokeRect(0, 0, gameboard.width, gameboard.height)
     gameboardCtx.drawImage(background,0,0)
-    gameboardCtx.drawImage(bird,10,10,30,30)
+    gameboardCtx.drawImage(bird,10,by,30,30)
 }
