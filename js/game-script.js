53aa874c0f9e9bb15192dbcc3de274111efb109d
Avery Burne <burneav@gmail.com>
2021-08-20 14:55:32 -0400

    Added pipe image to game

--- js/game-script.js
+++ js/game-script.js
@@ -2,10 +2,12 @@ const gameboard = document.getElementById('game-canvas')
 const gameboardCtx = gameboard.getContext('2d')
 const background = new Image()
 const bird = new Image()
+const pipe = new Image()
 let by = 200
 
 background.src = "js/images/flappy-bird-background.png"
 bird.src = "js/images/flappy-bird.png"
+pipe.src = 'js/image/flappy-bird-pipe.png'
 
 document.addEventListener("keydown", moveBird)
 
@@ -31,4 +33,5 @@ function draw() {
     // gameboardCtx.strokeRect(0, 0, gameboard.width, gameboard.height)
     gameboardCtx.drawImage(background,0,0)
     gameboardCtx.drawImage(bird,10,by,30,30)
+    gameboardCtx.drawImage(pipe,200,250)
 }
